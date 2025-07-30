f7a3155af97b71db0d65dd8b4fc86c62f56cd095
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-07-29 21:24:59 -0700

    Create location.jsx

---
+++ pages/location.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+import LocationSection from "./components/LocationMap"
+import Footer from "./components/Footer"
+
+export default function Location() {
+  return (
+    <div>
+        <Header />
+        <LocationSection />
+        <Footer />
+    </div>
+  )
+}
