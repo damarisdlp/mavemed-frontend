f325232167f732c90c6433b1d50744ddd49dea80
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-07-30 01:57:17 -0700

    Update aboutus.jsx

--- pages/aboutus.jsx
+++ pages/aboutus.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header"
-import Footer
- from "./components/Footer"
+import Footer from "./components/Footer"
+import AboutSection from "./components/AboutSection"
+import InstagramFeed from "./components/InstagramFeed"
+
 export default function AboutUs() {
   return (
     <section>
         <Header />
+        <AboutSection />
+        <InstagramFeed />
         <Footer />
     </section>
   )
