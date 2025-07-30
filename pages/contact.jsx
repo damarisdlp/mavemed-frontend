4162db947cadc325dfe57380352a6eb439978621
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-07-29 22:16:05 -0700

    Create contact.jsx

---
+++ pages/contact.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import ContactUs from "./components/ContactUs"
+
+export default function Contact() {
+  return (
+    <div>
+        <Header/>
+        <ContactUs/>
+        <Footer />
+    </div>
+  )
+}
