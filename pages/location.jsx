785658342780fd4ef360205b709a5f1f8fc17686
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-07-30 02:42:58 -0700

    Update location.jsx

--- pages/location.jsx
+++ pages/location.jsx
@@ -1,3 +1,4 @@
+import PromoBanner from "./components/PromoBanner";
 import Header from "./components/Header"
 import LocationSection from "./components/LocationSection"
 import Footer from "./components/Footer"
@@ -5,6 +6,7 @@ import Footer from "./components/Footer"
 export default function Location() {
   return (
     <div>
+      <PromoBanner/>
         <Header />
         <LocationSection />
         <Footer />
