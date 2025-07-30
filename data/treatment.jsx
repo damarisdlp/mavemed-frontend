80717cef531730cd17cc44cb6882dcb28fc9fee4
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-07-30 09:46:11 -0700

    Update treatment.jsx

---
+++ data/treatment.jsx
@@ -0,0 +1,30 @@
+export const treatment = [
+    {
+        name: "wrinkle reducers",
+        displayName: "Botox Wrinkle Reducers",
+        image: "/botox.jpg",
+        description: "Botox is",
+        standard: "",
+        promo: "",
+        note: "",
+        details: "",
+        goals: "",
+        treatableAreas: "",
+        pricing: [
+            {
+                zone:"",
+                standardPrice:"",
+                promoPrice:""
+            }            
+        ],
+        addOn:[
+            {
+                name: "",
+                standardPrice: "",
+                promoPrice:""
+            }
+        ]
+
+
+    }
+]
