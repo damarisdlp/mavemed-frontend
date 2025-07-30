0863414abe6979ca0a001b637eb73413e8114ac7
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-07-29 22:06:35 -0700

    Delete Layout.jsx

--- pages/components/Layout.jsx
+++
@@ -1,9 +0,0 @@
-// components/Layout.jsx
-
-export default function Layout({ children }) {
-  return (
-    <div className="min-h-screen w-full flex flex-col bg-white text-black overflow-x-hidden">
-      {children}
-    </div>
-  );
-}
