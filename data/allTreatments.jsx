19e7842ae43d05a93e0a99a0a7bb5d680ab2bc04
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-10-01 16:47:20 -0700

    Update allTreatments.jsx

--- data/allTreatments.jsx
+++ data/allTreatments.jsx
@@ -5,5 +5,14 @@ import { casmaraPurifying } from './treatments/facials/casmara/purifying';
 import { casmaraInfinity } from './treatments/facials/casmara/infinity';
 import { casmaraRetinolProAge } from './treatments/facials/casmara/retinol';
 import { janssenRadianceBoost } from './treatments/facials/janssen/radiance'
+import { janssenUltraRenewal } from './treatments/facials/janssen/ultra';
 
-export const allTreatments = [botox, hydrafacial, casmaraPurifying, casmaraInfinity, casmaraRetinolProAge,janssenRadianceBoost];
+export const allTreatments = [
+    botox, 
+    hydrafacial, 
+    casmaraPurifying, 
+    casmaraInfinity, 
+    casmaraRetinolProAge,
+    janssenRadianceBoost,
+    janssenUltraRenewal
+];
