6c8c3472f1cbeaca110b407727368f1c8708750d
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-10-02 00:03:51 -0700

    Create zury.jsx

---
+++ data/staff/receptionists/zury.jsx
@@ -0,0 +1,13 @@
+export const zury = {
+    name: "zury",
+    displayName: "Zury",
+    title: "Receptionist",
+    image: "/zury.jpg",
+    bio: "Zury is a cosmetologist",
+    favorites: [
+      {
+        serviceName: "HydraFacial MD - Face",
+        link: "/treatments/hydrafacial",
+      },
+    ],
+}
