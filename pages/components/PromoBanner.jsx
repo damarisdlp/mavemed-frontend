be7953d8a1ef455e333fd8a39321ec9c931bb71f
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-10-02 01:06:11 -0700

    Update PromoBanner.jsx

--- pages/components/PromoBanner.jsx
+++ pages/components/PromoBanner.jsx
@@ -4,7 +4,9 @@ export default function PromoBanner() {
   return (
     <div className="bg-[#731a2f] text-white text-sm text-center py-2 px-4">
       Access exclusive promo pricing on select treatments.{" "}
-      <a href="#contact" className="underline">
+      <a
+      href="https://wa.me/+526642077675"
+      className="underline">
         Inquire Now
       </a>
     </div>
