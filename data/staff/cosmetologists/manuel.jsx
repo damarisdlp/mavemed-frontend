b09a044ad7ce0f6a91787faa600259adf6395918
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-10-02 00:03:34 -0700

    Create manuel.jsx

---
+++ data/staff/cosmetologists/manuel.jsx
@@ -0,0 +1,13 @@
+export const manuel = {
+    name: "manuel",
+    displayName: "Manuel",
+    title: "Cosmetologist",
+    image: "/manuel.jpg",
+    bio: "Manuel is a cosmetologist",
+    favorites: [
+      {
+        serviceName: "HydraFacial MD - Face",
+        link: "/treatments/hydrafacial",
+      },
+    ],
+}
