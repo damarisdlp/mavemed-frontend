77718946d36953121d5de8d1921f9d54ccb55c23
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-10-02 00:03:49 -0700

    Create nataly.jsx

---
+++ data/staff/providers/nataly.jsx
@@ -0,0 +1,13 @@
+export const nataly = {
+    name: "nataly",
+    displayName: "Nataly",
+    title: "Provider",
+    image: "/nataly.jpg",
+    bio: "Nataly is a cosmetologist",
+    favorites: [
+      {
+        serviceName: "HydraFacial MD - Face",
+        link: "/treatments/hydrafacial",
+      },
+    ],
+}
