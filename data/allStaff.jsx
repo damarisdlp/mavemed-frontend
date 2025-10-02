227fe34b177dfdf82b7d6b245cb0ecbe5a050ffd
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-10-02 00:03:29 -0700

    Create allStaff.jsx

---
+++ data/allStaff.jsx
@@ -0,0 +1,19 @@
+import { vicky } from "./staff/cosmetologists/vicky"
+import { manuel } from "./staff/cosmetologists/manuel"
+import { mayra } from "./staff/cosmetologists/mayra"
+import { veronica } from "./staff/directors/veronica"
+import { damaris } from "./staff/directors/damaris"
+import { nataly } from "./staff/providers/nataly"
+import { jocelyn } from "./staff/providers/jocelyn"
+import { zury } from "./staff/receptionists/zury"
+
+export const allStaff = [
+    vicky,
+    manuel,
+    mayra,
+    veronica,
+    damaris,
+    nataly,
+    jocelyn,
+    zury
+]
