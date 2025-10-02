451d7ddcc11020380a758024831c45b4861388cd
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-10-02 00:03:36 -0700

    Create mayra.jsx

---
+++ data/staff/cosmetologists/mayra.jsx
@@ -0,0 +1,13 @@
+export const mayra = {
+    name: "mayra",
+    displayName: "Mayra",
+    title: "Cosmetologist",
+    image: "/mayra.jpg",
+    bio: "Mayra is a cosmetologist",
+    favorites: [
+      {
+        serviceName: "HydraFacial MD - Face",
+        link: "/treatments/hydrafacial",
+      },
+    ],
+}
