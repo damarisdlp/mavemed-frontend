0ba6d06d19b07d788d7ee918e9630cfed0a98a5e
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-10-02 00:03:38 -0700

    Create vicky.jsx

---
+++ data/staff/cosmetologists/vicky.jsx
@@ -0,0 +1,13 @@
+export const vicky = {
+    name: "vicky",
+    displayName: "Vicky",
+    title: "Cosmetologist",
+    image: "/vicky.jpg",
+    bio: "Vicky is a cosmetologist",
+    favorites: [
+      {
+        serviceName: "HydraFacial MD - Face",
+        link: "/treatments/hydrafacial",
+      },
+    ],
+}
