2be39cafc2dc416dfe8d27900b6b9c108e7d5280
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-10-02 00:03:42 -0700

    Create veronica.jsx

---
+++ data/staff/directors/veronica.jsx
@@ -0,0 +1,13 @@
+export const veronica = {
+    name: "veronica",
+    displayName: "Veronica",
+    title: "Director",
+    image: "/veronica.jpg",
+    bio: "Veronica is a director",
+    favorites: [
+      {
+        serviceName: "HydraFacial MD - Face",
+        link: "/treatments/hydrafacial",
+      },
+    ],
+}
