6643d5ee646e60b2543c70829066c7f1b082fa01
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-10-02 00:03:41 -0700

    Create damaris.jsx

---
+++ data/staff/directors/damaris.jsx
@@ -0,0 +1,13 @@
+export const damaris = {
+    name: "damaris",
+    displayName: "Damaris",
+    title: "Director",
+    image: "/damaris.jpg",
+    bio: "Damaris is a director",
+    favorites: [
+      {
+        serviceName: "HydraFacial MD - Face",
+        link: "/treatments/hydrafacial",
+      },
+    ],
+}
