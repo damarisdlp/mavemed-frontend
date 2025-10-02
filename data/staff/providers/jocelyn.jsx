885e7165900dee662b141db5f886a611113dc1ff
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-10-02 00:03:45 -0700

    Create jocelyn.jsx

---
+++ data/staff/providers/jocelyn.jsx
@@ -0,0 +1,13 @@
+export const jocelyn = {
+    name: "jocelyn",
+    displayName: "Jocelyn",
+    title: "Provider",
+    image: "/jocelyn.jpg",
+    bio: "Jocelyn is a cosmetologist",
+    favorites: [
+      {
+        serviceName: "HydraFacial MD - Face",
+        link: "/treatments/hydrafacial",
+      },
+    ],
+}
