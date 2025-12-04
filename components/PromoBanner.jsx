44bba04b20983f4d572adde79d78e08328e45b3e
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-12-04 11:52:25 -0600

    Update PromoBanner.jsx

--- components/PromoBanner.jsx
+++ components/PromoBanner.jsx
@@ -2,7 +2,7 @@
 import { useTranslation } from "next-i18next";
 
 export default function PromoBanner() {
-  const { t } = useTranslation("common");
+  const { t } = useTranslation("layout");
 
   return (
     <div className="bg-[#731a2f] text-white text-sm text-center py-2 px-4">
