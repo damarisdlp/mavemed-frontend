1cfa07c93612afc41fc7e0f03e7c43fa296693cd
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-12-03 23:11:53 -0600

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,13 @@
+const path = require("path");
+
 module.exports = {
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es'],
+    defaultLocale: "en",
+    locales: ["en", "es"],
   },
-  localePath: './public/locales',
-}
+  ns: ["common"],
+  fallbackLng: "en",
+  defaultNS: "common",
+  localePath: path.resolve("./public/locales"),
+  reloadOnPrerender: process.env.NODE_ENV === "development",
+};
