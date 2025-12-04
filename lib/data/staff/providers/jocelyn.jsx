d197f900ad0078fa1360a69af3d446c68ff7ed69
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-12-03 23:43:44 -0600

    Update jocelyn.jsx

--- lib/data/staff/providers/jocelyn.jsx
+++ lib/data/staff/providers/jocelyn.jsx
@@ -1,14 +1,29 @@
 export const jocelyn = {
-    name: "drjocelyn",
-    displayName: "Dr. Jocelyn",
-    category: "Providers",
-    title: "Provider",
-    image: "/drjocelyn.jpg",
-    bio: "Dr.Jocelyn is a Doctor",
-    favorites: [
-      {
-        serviceName: "HydraFacial MD - Face",
-        link: "/treatments/hydrafacial",
+  name: "drjocelyn",
+  displayName: {
+    en: "Dr. Jocelyn",
+    es: "Dra. Jocelyn"
+  },
+  category: {
+    en: "Providers",
+    es: "Medicos"
+  },
+  title: {
+    en: "Provider",
+    es: "Medicos"
+  },
+  image: "/drjocelyn.jpg",
+  bio: {
+    en: "Dr. Jocelyn is a doctor.",
+    es: "La doctora Jocelyn es doctora."
+  },
+  favorites: [
+    {
+      serviceName: {
+        en: "HydraFacial MD – Face",
+        es: "HydraFacial MD – Rostro"
       },
-    ],
-}
+      link: "/treatments/hydrafacial"
+    }
+  ]
+};
