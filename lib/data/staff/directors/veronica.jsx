bfd21fa27590c40e9e5187a0a11d070d9f5f2793
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-12-03 23:43:41 -0600

    Update veronica.jsx

--- lib/data/staff/directors/veronica.jsx
+++ lib/data/staff/directors/veronica.jsx
@@ -1,14 +1,29 @@
 export const veronica = {
-    name: "dr.veronica",
-    displayName: "Dr. Veronica",
-    category: "Directors",
-    title: "Director & Founder",
-    image: "/drveronica.jpg",
-    bio: "Dr.Veronica is a director",
-    favorites: [
-      {
-        serviceName: "HydraFacial MD - Face",
-        link: "/treatments/hydrafacial",
+  name: "dr.veronica",
+  displayName: {
+    en: "Dr. Veronica",
+    es: "Dra. Verónica"
+  },
+  category: {
+    en: "Directors",
+    es: "Directoras"
+  },
+  title: {
+    en: "Director & Founder",
+    es: "Directora y fundadora"
+  },
+  image: "/drveronica.jpg",
+  bio: {
+    en: "Dr. Veronica is a director.",
+    es: "La doctora Verónica es directora."
+  },
+  favorites: [
+    {
+      serviceName: {
+        en: "HydraFacial MD – Face",
+        es: "HydraFacial MD – Rostro"
       },
-    ],
-}
+      link: "/treatments/hydrafacial"
+    }
+  ]
+};
