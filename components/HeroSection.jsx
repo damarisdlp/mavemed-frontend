9474b26fcd5f3706319cf0ecb67eec2e94b51aad
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-12-03 23:28:40 -0600

    Update HeroSection.jsx

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,21 +1,26 @@
 // components/HeroSection.jsx
+import { useTranslation } from "next-i18next";
 
 export default function HeroSection() {
+  const { t } = useTranslation("common");
+
   return (
-<section className="bg-white min-h-[65vh] md:min-h-[75vh] flex flex-col justify-center items-center px-6 py-6 text-center relative">
-  <h1 className="text-4xl text-black sm:text-5xl md:text-6xl font-serif font-medium leading-tight max-w-4xl">
-        Mave Medical Spa in Tijuana 
+    <section className="bg-white min-h-[65vh] md:min-h-[75vh] flex flex-col justify-center items-center px-6 py-6 text-center relative">
+      <h1 className="text-4xl text-black sm:text-5xl md:text-6xl font-serif font-medium leading-tight max-w-4xl">
+        {t("heroSection.heading1")}
       </h1>
-      <h1 className="text-4xl text-black sm:text-4xl md:text-4xl font-serif font-medium leading-tight max-w-4xl">Botox, Fillers, RF Microneedling & More</h1>
+      <h1 className="text-4xl text-black sm:text-4xl md:text-4xl font-serif font-medium leading-tight max-w-4xl">
+        {t("heroSection.heading2")}
+      </h1>
       <p className="text-base sm:text-lg text-gray-600 mt-4 max-w-lg">
-        Cross the border for natural-looking results. We specialize in Botox, Sculptra, dermal fillers, PDO threads, radiofrequency microneedling, and laser treatments — trusted by clients from San Diego, Los Angeles, Orange County, Tijuana and beyond.
+        {t("heroSection.copy")}
       </p>
       <div className="mt-8 mb-4">
         <a
           href="https://wa.me/+526642077675"
           className="bg-black text-white px-8 py-3 rounded-full hover:bg-[#731a2f] transition active:scale-95"
         >
-          Book Your Consultation
+          {t("heroSection.cta")}
         </a>
       </div>
     </section>
