14f6ff0f91ebc45ab7c2a75c7d197337e33331b5
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-12-03 23:59:03 -0600

    Update nataly.jsx

--- lib/data/staff/providers/nataly.jsx
+++ lib/data/staff/providers/nataly.jsx
@@ -1,14 +1,29 @@
 export const nataly = {
-    name: "drnataly",
-    displayName: "Dr. Nataly",
-    category: "Providers",
-    title: "Provider",
-    image: "/drnataly.jpg",
-    bio: "Dr. Nataly is a Doctor",
-    favorites: [
-      {
-        serviceName: "HydraFacial MD - Face",
-        link: "/treatments/hydrafacial",
+  name: "drnataly",
+  displayName: {
+    en: "Dr. Nataly",
+    es: "Dra. Nataly"
+  },
+  category: {
+    en: "Providers",
+    es: "Medicos"
+  },
+  title: {
+    en: "Provider",
+    es: "Medicos"
+  },
+  image: "/drnataly.jpg",
+  bio: {
+    en: "Dr. Nataly is a doctor.",
+    es: "La doctora Nataly es doctora."
+  },
+  favorites: [
+    {
+      serviceName: {
+        en: "HydraFacial MD – Face",
+        es: "HydraFacial MD – Rostro"
       },
-    ],
-}
+      link: "/treatments/hydrafacial"
+    }
+  ]
+};
