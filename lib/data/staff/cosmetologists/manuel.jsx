f799bfc9f8c645fac5793dac4dea3d3c52649f96
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-12-03 23:19:53 -0600

    Update manuel.jsx

--- lib/data/staff/cosmetologists/manuel.jsx
+++ lib/data/staff/cosmetologists/manuel.jsx
@@ -1,14 +1,29 @@
 export const manuel = {
-    name: "manuel",
-    displayName: "Manuel",
-    category: "Cosmetologists",
-    title: "Cosmetologist",
-    image: "/manuel.jpg",
-    bio: "Manuel is a cosmetologist",
-    favorites: [
-      {
-        serviceName: "HydraFacial MD - Face",
-        link: "/treatments/hydrafacial",
+  name: "manuel",
+  displayName: {
+    en: "Manuel",
+    es: "Manuel"
+  },
+  category: {
+    en: "Cosmetologists",
+    es: "Cosmetólogos"
+  },
+  title: {
+    en: "Cosmetologist",
+    es: "Cosmetólogo"
+  },
+  image: "/manuel.jpg",
+  bio: {
+    en: "Manuel is a cosmetologist.",
+    es: "Manuel es un cosmetólogo."
+  },
+  favorites: [
+    {
+      serviceName: {
+        en: "HydraFacial MD – Face",
+        es: "HydraFacial MD – Rostro"
       },
-    ],
-}
+      link: "/treatments/hydrafacial"
+    }
+  ]
+};
