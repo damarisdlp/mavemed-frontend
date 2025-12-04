b51e960dd06d4927b17ae7f179e4cdac530eb3a7
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-12-03 23:28:58 -0600

    Update mayra.jsx

--- lib/data/staff/cosmetologists/mayra.jsx
+++ lib/data/staff/cosmetologists/mayra.jsx
@@ -1,14 +1,29 @@
 export const mayra = {
-    name: "mayra",
-    displayName: "Mayra",
-    category: "Cosmetologists",
-    title: "Cosmetologist",
-    image: "/mayra.jpg",
-    bio: "Mayra is a cosmetologist",
-    favorites: [
-      {
-        serviceName: "HydraFacial MD - Face",
-        link: "/treatments/hydrafacial",
+  name: "mayra",
+  displayName: {
+    en: "Mayra",
+    es: "Mayra"
+  },
+  category: {
+    en: "Cosmetologists",
+    es: "Cosmetólogas"
+  },
+  title: {
+    en: "Cosmetologist",
+    es: "Cosmetóloga"
+  },
+  image: "/mayra.jpg",
+  bio: {
+    en: "Mayra is a cosmetologist.",
+    es: "Mayra es una cosmetóloga."
+  },
+  favorites: [
+    {
+      serviceName: {
+        en: "HydraFacial MD – Face",
+        es: "HydraFacial MD – Rostro"
       },
-    ],
-}
+      link: "/treatments/hydrafacial"
+    }
+  ]
+};
