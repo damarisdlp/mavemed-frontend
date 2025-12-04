fd6240faeb35f3317e754e96089cf74a20c96de3
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-12-03 22:22:08 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import "@/styles/globals.css";
-import { appWithTranslation } from 'next-i18next';
+import { appWithTranslation } from "next-i18next";
+import nextI18NextConfig from "../next-i18next.config";
 
 function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-export default appWithTranslation(App);
+export default appWithTranslation(App, nextI18NextConfig);
