147e5f55a649bdd2a4339466ffa40eaeb2d7f34d
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-12-03 23:28:48 -0600

    Update PromoBanner.jsx

--- components/PromoBanner.jsx
+++ components/PromoBanner.jsx
@@ -1,13 +1,14 @@
 // components/PromoBanner.jsx
+import { useTranslation } from "next-i18next";
 
 export default function PromoBanner() {
+  const { t } = useTranslation("common");
+
   return (
     <div className="bg-[#731a2f] text-white text-sm text-center py-2 px-4">
-      Access exclusive promo pricing on select treatments.{" "}
-      <a
-      href="https://wa.me/+526642077675"
-      className="underline">
-        Inquire Now
+      {t("promoBanner.text")}{" "}
+      <a href="https://wa.me/+526642077675" className="underline">
+        {t("promoBanner.cta")}
       </a>
     </div>
   );
