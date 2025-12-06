1b0108ee0a04da0e4dcff6feb2353c4b4280ff06
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-12-06 10:22:01 -0800

    Update botox.jsx

--- lib/data/treatments/wrinkleReducers/botox.jsx
+++ lib/data/treatments/wrinkleReducers/botox.jsx
@@ -144,48 +144,28 @@ export const botox = {
       optionName: {
         en: "Hydrafacial MD - Face",
         es: "Hydrafacial MD - Rostro"
-      },
-      displayName: {
-        en: "HydraFacial MD - Face",
-        es: "HydraFacial MD - Rostro"
-      },
-      link: "/treatments/hydrafacial"
+      }
     },
     {
       treatmentSlug: "casmara-purifying",
       optionName: {
         en: "Casmara Purifying Algae Facial",
         es: "Facial Casmara Purificante de Algas"
-      },
-      displayName: {
-        en: "Casmara Purifying Algae Facial",
-        es: "Facial Casmara Purificante de Algas"
-      },
-      link: "/treatments/casmara-purifying"
+      }
     },
     {
       treatmentSlug: "casmara-infinity",
       optionName: {
         en: "Casmara Infinity Anti‑Aging Facial",
         es: "Facial Casmara Antienvejecimiento Infinity"
-      },
-      displayName: {
-        en: "Casmara Infinity Anti‑Aging Facial",
-        es: "Tratamiento Facial Anti-Envejecimiento Casmara Infinity"
-      },
-      link: "/treatments/casmara-infinity"
+      }
     },
     {
       treatmentSlug: "casmara-retinol-proage",
       optionName: {
         en: "Casmara Retinol Pro‑Age Facial",
         es: "Casmara Retinol Pro‑Age Facial"
-      },
-      displayName: {
-        en: "Casmara Retinol Pro‑Age Facial",
-        es: "Tratamiento Facial Retinol Pro-Edad Casmara"
-      },
-      link: "/treatments/casmara-retinol-proage"
+      }
     },
   ],
   expectations: {
