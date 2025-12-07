36c95f41d3e9e3f4726613b5ad6d11f8ee820b7b
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-12-06 16:55:55 -0800

    Update harmonyCa.jsx

--- lib/data/treatments/collagenBiostimulators/harmonyCa.jsx
+++ lib/data/treatments/collagenBiostimulators/harmonyCa.jsx
@@ -118,8 +118,8 @@ export const harmonyca = {
     {
       treatmentSlug: "casmara-infinity",
       optionName: {
-        en: "Casmara Infinity Anti‑Aging Facial",
-        es: "Facial Casmara Antienvejecimiento Infinity"
+        en: "Casmara - Infinity Anti‑Aging Facial",
+        es: "Casmara - Facial Antienvejecimiento Infinity"
       }
     },
     {
