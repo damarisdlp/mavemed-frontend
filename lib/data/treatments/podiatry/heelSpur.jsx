0b43fb71eede2d98f0de921473ef51e35d0873b7
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-12-06 16:56:12 -0800

    Update heelSpur.jsx

--- lib/data/treatments/podiatry/heelSpur.jsx
+++ lib/data/treatments/podiatry/heelSpur.jsx
@@ -94,7 +94,7 @@ export const calcanealSpur = {
     },
     {
       treatmentSlug: "casmara-retinol-proage",
-      optionName: { en: "Casmara Retinol Pro‑Age Facial", es: "Casmara Retinol Pro‑Age Facial" }
+      optionName: { en: "Casmara - Retinol Pro‑Age Facial", es: "Casmara - Facial Retinol Pro‑Age" }
     },
     {
       treatmentSlug: "swedish-massage",
