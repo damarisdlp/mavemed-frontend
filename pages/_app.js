d3270e105e030c33b458e69c065220d034cff05b
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-12-14 06:02:07 -0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import "@/styles/globals.css";
 import { appWithTranslation } from "next-i18next";
 import nextI18NextConfig from "../next-i18next.config";
+import ScrollToTopButton from "@/components/ScrollToTop";
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ScrollToTopButton />
+    </>
+  );
 }
 
 export default appWithTranslation(App, nextI18NextConfig);
