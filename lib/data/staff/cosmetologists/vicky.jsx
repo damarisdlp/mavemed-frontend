7c8e329427fae1998c51f844c504e1034a450882
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2025-12-14 08:09:28 -0800

    Update vicky.jsx

--- lib/data/staff/cosmetologists/vicky.jsx
+++ lib/data/staff/cosmetologists/vicky.jsx
@@ -14,16 +14,12 @@ export const vicky = {
   },
   image: "/vicky.jpg",
   bio: {
-    en:"Vicky Palomares is a licensed cosmetologist with nine years at Mave. Trained at the Instituto de Cosmiatría Estética, she is also fully certified in laser hair removal, radiofrequency, K-Laser treatments, Ultraformer, and manual facial techniques. Vicky loves pairing Hydrafacial with customized treatment plans so patients leave glowing and confident. Patients appreciate her gentle guidance and the way she introduces advanced treatments like microneedling when they can truly benefit the skin. Outside the clinic, she enjoys cooking and spending relaxing moments with her loved ones.",    
+    en:"Vicky Palomares is a licensed cosmetologist with nine years at Mave. Trained at the Instituto de Cosmiatría Estética, she is also fully certified in laser hair removal, radiofrequency, K-Laser treatments, Ultraformer MPT, and manual facial techniques. Vicky loves pairing Hydrafacial with customized treatment plans so patients leave glowing and confident. Patients appreciate her gentle guidance and the way she introduces advanced treatments like microneedling when they can truly benefit the skin. Outside the clinic, she enjoys cooking and spending relaxing moments with her loved ones.",    
     es: "Vicky Palomares es cosmetóloga certificada con nueve años de experiencia en Mave. Formada en el Instituto de Cosmiatría Estética, también está capacitada en depilación láser, radiofrecuencia, K-Laser, Ultraformer y técnicas de faciales manuales. Vicky disfruta combinar Hydrafacial con planes de tratamiento personalizados para que cada paciente salga con la piel luminosa y mayor confianza. Los pacientes valoran su guía suave y la manera en que introduce tratamientos avanzados como el microneedling cuando realmente benefician la salud de la piel. Fuera de la clínica, le encanta cocinar y disfrutar momentos de descanso con sus seres queridos."
   },
   favorites: [
     {
       treatmentSlug: "mesotherapy-infusions",
-      optionName: {
-        en: "Mesotherapy - PDRN - Polydeoxyribonucleotide",
-        es: "Mesoterapia - PDRN - Polidesoxirribonucleótido"
-      }
     }
   ]
 };
