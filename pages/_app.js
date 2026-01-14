a98a92eab68b6c3d0ba951a245d483cf87000495
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2026-01-13 17:50:19 -0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,19 @@ import "@/styles/globals.css";
 import { appWithTranslation } from "next-i18next";
 import nextI18NextConfig from "../next-i18next.config";
 import ScrollToTopButton from "@/components/ScrollToTop";
+import { Manrope } from "next/font/google";
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 function App({ Component, pageProps }) {
   return (
-    <>
+    <div className={manrope.className}>
       <Component {...pageProps} />
       <ScrollToTopButton />
-    </>
+    </div>
   );
 }
 
