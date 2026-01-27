bb18c49d6ddb200e9462644727436aeead6be527
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2026-01-26 16:08:56 -0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/logo-mave.png" type="image/png" />
+        <link rel="icon" href="/site_icon.png" type="image/png" />
         <link rel="apple-touch-icon" href="/site_icon.png" />
         <script
           async
