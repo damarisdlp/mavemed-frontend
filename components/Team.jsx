b2eb9bd94c32937c1509cb8ad0ef18558c8d027e
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2026-01-26 18:01:19 -0800

    Update Team.jsx

--- components/Team.jsx
+++ components/Team.jsx
@@ -68,7 +68,7 @@ export default function Team() {
 
   return (
     <div className="bg-white scroll-smooth relative">
-      <div className="w-full max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 pt-12 pb-4">
+      <div className="w-full max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 pb-4">
         {/* Sticky Category Menu */}
         <div
           id="category-menu"
