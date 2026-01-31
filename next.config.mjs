864c3aceabc8093cb6a8f16dbdfa3a3bf50891f4
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2026-01-31 15:24:21 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,30 @@ const nextConfig = {
     defaultLocale: "en",
     locales: ["en", "es"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/sculptra_candidacy",
+        destination: "/sculptra-candidacy",
+        permanent: true,
+      },
+      {
+        source: "/es/sculptra_candidacy",
+        destination: "/es/sculptra-candidacy",
+        permanent: true,
+      },
+      {
+        source: "/sculptra-rf_candidacy",
+        destination: "/sculptra-rf-candidacy",
+        permanent: true,
+      },
+      {
+        source: "/es/sculptra-rf_candidacy",
+        destination: "/es/sculptra-rf-candidacy",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
