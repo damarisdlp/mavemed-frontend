3a2eb3157bbda1ef5af9d56d0b1fdcbd8bdf3017
Damaris De La Paz <86942528+damarisdlp@users.noreply.github.com>
2026-03-23 21:35:05 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        />
         <meta name="robots" content="index, follow" />
         <meta property="og:site_name" content="Mave Medical Spa" />
         <meta name="twitter:card" content="summary_large_image" />
